55634ab15608b69c6a1675c510fd8c2c315b792d
Junior <lucio.jose.t.junior@accenture.com>
2020-08-23 16:17:44 -0300

    23082020

--- EBS_applicacoes.sql
+++
@@ -1,8 +0,0 @@
-select */*,a.application_name "Application Name",
-       decode(b.status, 'I', 'Installed', 'S', 'Shared', 'UnInstalled') "Status",
-       PATCH_LEVEL "Patch Level",
-       a.basepath "Basepath"*/
-  from APPS.fnd_application_vl a, APPS.fnd_product_installations b
- where a.application_id = b.application_id
- order by 2, 1
-
